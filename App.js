b021f322f7bd25c22595041407c218bf9ce7aa99
valentineejk <valentineejk@gmail.com>
2023-03-12 17:25:00 +0100

    1.0.0

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import ProductsScreen from './src/screens/productsScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+  
+      <ProductsScreen/>
+ 
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
---
+++ src/screens/productsScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from "../data/products";
+
+const ProductsScreen = () => {
+    return (
+        <View>
+            <FlatList
+                numColumns={ 2 }
+                data={ products }
+                renderItem={ ({ item }) => (
+                    <View style={ styles.imageCn }>
+                        <Image
+
+                            source={ {
+                                uri: item.image,
+                            } }
+                            style={ styles.image }
+                        />
+                    </View>
+
+                ) }
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        aspectRatio: 1
+    },
+    imageCn: {
+        width: "50%",
+        padding: 1,
+
+    }
+})
+
+export default ProductsScreen;
