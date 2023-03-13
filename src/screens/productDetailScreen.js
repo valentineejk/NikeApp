b99b13b3febd7d7acef3ac058592df5f2cb982f6
valentineejk <valentineejk@gmail.com>
2023-03-13 13:10:22 +0100

    products detail

--- src/screens/productDetailScreen.js
+++ src/screens/productDetailScreen.js
@@ -1,60 +1,72 @@
-import { StyleSheet, View, Image, FlatList, useWindowDimensions, Text, ScrollView } from "react-native";
+import { StyleSheet, View, Image, FlatList, useWindowDimensions, Text, ScrollView, Pressable } from "react-native";
 import products from "../data/products";
 
 const ProductDetailsScreen = () => {
     const product = products[0];
     const { width } = useWindowDimensions();
+    const addtocart = () => {
+            console.warn("added");
+                            }
+
+
+
     return (
         <View>
             <ScrollView
-            showsVerticalScrollIndicator="false"
+                showsVerticalScrollIndicator="false"
             >
-            {/* Image Carousel */ }
-            <FlatList
-            
-            horizontal
-            pagingEnabled
-                showsHorizontalScrollIndicator="false"
-                // numColumns={ 2 }
-                data={ product.images }
-                renderItem={ ({ item }) => (
+                {/* Image Carousel */ }
+                <FlatList
 
-                    <Image
+                    horizontal
+                    pagingEnabled
+                    showsHorizontalScrollIndicator="false"
+                    // numColumns={ 2 }
+                    data={ product.images }
+                    renderItem={ ({ item }) => (
 
-                        source={ {
-                            uri: item,
-                        } }
-                        style={{
-                            width,
-                            aspectRatio: 1,
-                        } }
-                    />
+                        <Image
+
+                            source={ {
+                                uri: item,
+                            } }
+                            style={ {
+                                width,
+                                aspectRatio: 1,
+                            } }
+                        />
 
 
-                ) }
-            />
-            <View style={{
-                padding: 20
-            }}>
-            {/* Title */ }
-            <Text style={styles.title}>
-                { product.name}
-            </Text>
+                    ) }
+                />
+                <View style={ {
+                    padding: 20
+                } }>
+                    {/* Title */ }
+                    <Text style={ styles.title }>
+                        { product.name }
+                    </Text>
 
-            {/* Price */ }
-            <Text style={ styles.price }>
-                ${ product.price }
-            </Text>
+                    {/* Price */ }
+                    <Text style={ styles.price }>
+                        ${ product.price }
+                    </Text>
 
-            {/* Description */ }
-            <Text style={ styles.description }>
-                { product.description }
-            </Text>
-            </View>
+                    {/* Description */ }
+                    <Text style={ styles.description }>
+                        { product.description }
+                    </Text>
+                </View>
+            </ScrollView>
+
             {/* Add to cart button */ }
+            <Pressable style={ styles.button } onPress={addtocart } >
+                <Text style={ styles.buttonTxt }>
+                    Add to cart
+                </Text>
+            </Pressable>
 
             {/* Navigation icon */ }
-            </ScrollView>
         </View>
     );
 };
@@ -82,6 +94,25 @@ const styles = StyleSheet.create({
         lineHeight: 25,
         fontWeight: "300",
     },
+    button: {
+        position: 'absolute',
+        backgroundColor: 'black',
+        bottom: 50,
+        width: '70%',
+        alignSelf: 'center',
+        borderRadius: 100,
+        padding: 20,
+        alignItems: 'center',
+
+    },
+    buttonTxt: {
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16,
+        // alignSelf: 'center',
+
+    }
+
 });
 
 export default ProductDetailsScreen;
